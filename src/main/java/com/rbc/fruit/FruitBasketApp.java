4c29a90c368c59e9c856480f69e67fe3c96cdb86
marc <marcedwards1977@gmail.com>
2016-06-30 08:09:22 +0100

    Fruit basket

--- src/main/java/com/rbc/fruit/FruitBasketApp.java
+++ src/main/java/com/rbc/fruit/FruitBasketApp.java
@@ -1,16 +1,12 @@
 package com.rbc.fruit;
 
 
-import javafx.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.stream.Collectors;
 
 public class FruitBasketApp {
 
