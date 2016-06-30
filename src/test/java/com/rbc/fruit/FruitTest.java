44deaf6bbe0c3c89e9b8b3e6115a07acb3105cc4
marc <marcedwards1977@gmail.com>
2016-06-30 08:11:14 +0100

    Fruit basket - removed spring runner

--- src/test/java/com/rbc/fruit/FruitTest.java
+++ src/test/java/com/rbc/fruit/FruitTest.java
@@ -3,14 +3,9 @@ package com.rbc.fruit;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/fruit-basket-context.xml"})
 public class FruitTest {
 
     private static Logger log = LoggerFactory.getLogger(FruitTest.class);
